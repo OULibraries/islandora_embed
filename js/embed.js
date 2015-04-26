6ec2f51660bb4c608b49afa9ac6c85cf0680403a
Tao Zhao <tao.zhao@ou.edu>
2015-04-26 04:42:41 +0000

    phpcbf Drupal formatting changes

--- js/embed.js
+++ js/embed.js
@@ -6,50 +6,50 @@
 
 (function ($) {
     
-   /**
+    /**
    * Modify the display of embedded islandora bookreader.
    */
     Drupal.behaviors.embedBookReader = {
-    attach: function(context, settings) {
+        attach: function(context, settings) {
       
-        var shareLink = $(".BRicon.share.cboxElement");
-        if(shareLink){
-            shareLink.remove();
-        }
+            var shareLink = $(".BRicon.share.cboxElement");
+            if(shareLink) {
+                shareLink.remove();
+            }
         
-        var spanBRreturn = $("span#BRreturn");
-        if(spanBRreturn){
-            spanBRreturn.remove();
-        }
+            var spanBRreturn = $("span#BRreturn");
+            if(spanBRreturn) {
+                spanBRreturn.remove();
+            }
         
-        // make the title font size smaller:
-        var bookTitle = $("h1#page-title");
-        if(bookTitle){
-            bookTitle.css("font-size", "16px");
-        }
+            // make the title font size smaller:
+            var bookTitle = $("h1#page-title");
+            if(bookTitle) {
+                bookTitle.css("font-size", "16px");
+            }
         
-        var bookSearchForm = $("#textSrch");
-        if(bookSearchForm){
-            bookSearchForm.remove();
-        }
+            var bookSearchForm = $("#textSrch");
+            if(bookSearchForm) {
+                bookSearchForm.remove();
+            }
         
-        var bookBRnavpos = $("div#BRnavpos");
-        if(bookBRnavpos){
-            bookBRnavpos.remove();
-        }
+            var bookBRnavpos = $("div#BRnavpos");
+            if(bookBRnavpos) {
+                bookBRnavpos.remove();
+            }
         
-        // when display the book reader in the two page view mode, the BRtwopageview div top is set wrong:
-        var bookBRtwopageview = $("div#BRtwopageview");
-        if(bookBRtwopageview){
-            bookBRtwopageview.css("top", "35px");
-        }
+            // when display the book reader in the two page view mode, the BRtwopageview div top is set wrong:
+            var bookBRtwopageview = $("div#BRtwopageview");
+            if(bookBRtwopageview) {
+                bookBRtwopageview.css("top", "35px");
+            }
         
-        // adjust the height of the book reader based on the iframe height:
-        var height = Drupal.settings.islandora_bookreader_embed.height;
-        var bookBRcontainer = $("div#BRcontainer");
-        if(bookBRcontainer){
-            bookBRcontainer.css("height", parseInt(height)-112);
+            // adjust the height of the book reader based on the iframe height:
+            var height = Drupal.settings.islandora_bookreader_embed.height;
+            var bookBRcontainer = $("div#BRcontainer");
+            if(bookBRcontainer) {
+                bookBRcontainer.css("height", parseInt(height)-112);
+            }
         }
-      }
-  }
+    }
 })(jQuery);
