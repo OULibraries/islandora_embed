37cc0d18438be8e6078257ebf6d36a02381dc8d0
Logan Cox <lmc@ou.edu>
2015-05-08 14:38:31 -0500

    removed ide licensing boilerplate

--- js/embed.js
+++ js/embed.js
@@ -1,49 +1,43 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 (function ($) {
-    
+
    /**
    * Modify the display of embedded islandora bookreader.
    */
     Drupal.behaviors.embedBookReader = {
     attach: function(context, settings) {
-      
+
         var shareLink = $(".BRicon.share.cboxElement");
         if(shareLink){
             shareLink.remove();
         }
-        
+
         var spanBRreturn = $("span#BRreturn");
         if(spanBRreturn){
             spanBRreturn.remove();
         }
-        
+
         // make the title font size smaller:
         var bookTitle = $("h1#page-title");
         if(bookTitle){
             bookTitle.css("font-size", "16px");
         }
-        
+
         var bookSearchForm = $("#textSrch");
         if(bookSearchForm){
             bookSearchForm.remove();
         }
-        
+
         var bookBRnavpos = $("div#BRnavpos");
         if(bookBRnavpos){
             bookBRnavpos.remove();
         }
-        
+
         // when display the book reader in the two page view mode, the BRtwopageview div top is set wrong:
         var bookBRtwopageview = $("div#BRtwopageview");
         if(bookBRtwopageview){
             bookBRtwopageview.css("top", "35px");
         }
-        
+
         // adjust the height of the book reader based on the iframe height:
         var height = Drupal.settings.islandora_bookreader_embed.height;
         var bookBRcontainer = $("div#BRcontainer");
